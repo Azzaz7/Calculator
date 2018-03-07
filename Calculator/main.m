7473891f75853135cf3260722d68e7e244703ed3
azzaz <azzaz@azzazs-Mac.local>
2018-03-07 12:21:38 +0200

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by azzaz on 3/7/18.
+//  Copyright © 2018 azzaz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
